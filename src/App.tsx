197320bb140b31b0b2720a7dc7646bf00211ce9e
shupopo-999 <shu999.20099@gmail.com>
2024-12-18 17:55:01 +0900

    一旦

--- src/App.tsx
+++ src/App.tsx
@@ -64,7 +64,7 @@ function App() {
   return (
     <div className="App">
       <div>
-        <h2>Todoリスト with Typescript</h2>
+        <h2>Todoリスト</h2>
         <form onSubmit={(e) => handleSubmid(e)}>
           <input type="text" onChange={(e) => handlechange(e)} className="inputText"/>
           <input type="submit" value="作成" className="submitButton"/>
